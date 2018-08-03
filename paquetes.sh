2bb0b7c178621807f4c1bbc78ccfb238b833c38e
altabox <42061893+altabox@users.noreply.github.com>
2018-08-03 17:57:56 +0200

    Update paquetes.sh

--- paquetes.sh
+++ paquetes.sh
@@ -1,1 +1,2 @@
-sudo vim mc x11vnc pulseaudio pavucontrol wmctrl mplayer libgtkmm-2.4 libwebkitgtk-1.0 bcrypt mcrypt zip unzip ntp ntpdate htop openvpn arandr iftop
+libgtkmm-2.4 sudo vim mc x11vnc pulseaudio pavucontrol wmctrl mplayer libwebkitgtk-1.0 bcrypt mcrypt zip unzip ntp ntpdate htop openvpn arandr iftop
+#popo
