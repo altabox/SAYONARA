139a7c3ac831b5684c3431ddeee2fa7e6364c606
altabox <42061893+altabox@users.noreply.github.com>
2018-08-03 18:22:50 +0200

    Create paquete.sh

---
+++ paquete.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+LIST_OF_APPS="libwebkitgtk-1.0 sudo vim mc x11vnc pulseaudio pavucontrol wmctrl mplayer libgtkmm-2.4 bcrypt mcrypt zip unzip ntp ntpdate htop openvpn arandr iftop"
+
+apt-get update
+apt-get install -y $LIST_OF_APPS
