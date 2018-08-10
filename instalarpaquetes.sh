edaf552d78483bb38764bb0aba7426c229862523
altabox <42061893+altabox@users.noreply.github.com>
2018-08-10 17:50:37 +0200

    Update instalarpaquetes.sh

--- instalarpaquetes.sh
+++ instalarpaquetes.sh
@@ -9,7 +9,7 @@ if ! which teamviewer > /dev/null; then
    echo -e "Paquete no encontrado ¿Instalar? (y/n) \c"
    read
    if "$REPLY" = "y"; then
-      sudo apt-get install teamviewer
+      apt-get install teamviewer
    fi
    
 fi
